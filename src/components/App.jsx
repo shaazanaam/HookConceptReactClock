177d13781494d799cce9e1784a164b9ad3ad0d9e
Shaaz Anaam <anaamsz@mail.uc.edu>
2024-11-02 10:21:29 -0500

    time clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React ,{useState}from "react";
 
 function App() {
+
+  setInterval(realTime,1000) ;
+  const now = new Date().toTimeString().slice(0,8);
+   const [state,setTime] =useState(now);
+   console.log(state) 
+   
+  // function callTime(){
+  //   setTime (new Date().toLocaleTimeString().slice(0,8))
+  // }
+  function realTime(){
+    setTime(new Date().toLocaleTimeString().slice(0,8));
+  }
+
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{state}</h1>
+      <button onClick ={realTime}>Get Time</button>
     </div>
   );
 }
